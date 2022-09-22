63e51ecadc6f3631575c5ee2188f9174eb611f20
SnowyyUwU <ulisesdraws@gmail.com>
2022-09-22 00:58:42 +0000

    update

---
+++ src/main/java/com/snowy/snowycosmetics/CosmeticListener.java
@@ -0,0 +1,17 @@
+package com.snowy.snowycosmetics;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class CosmeticListener implements Listener {
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        if (e.getInventory() != null && e.getCurrentItem() != null) {
+            if (e.getView().getTitle().endsWith("Cosmetics Menu!")) {
+
+            }
+        }
+    }
+}
--- src/main/java/com/snowy/snowycosmetics/SnowyCosmetics.java
+++ src/main/java/com/snowy/snowycosmetics/SnowyCosmetics.java
@@ -1,12 +1,14 @@
 package com.snowy.snowycosmetics;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SnowyCosmetics extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getCommand("cosmetics").setExecutor(new CosmeticsCommand());
 
+        Bukkit.getPluginManager().registerEvents(new CosmeticListener(), this);
     }
 }
