612ef2f9719219fe64a1eedcd0cbc9110279abb5
SnowyyUwU <ulisesdraws@gmail.com>
2022-09-22 00:02:02 +0000

    Initial commit

---
+++ src/main/java/com/snowy/snowycosmetics/SnowyCosmetics.java
@@ -0,0 +1,12 @@
+package com.snowy.snowycosmetics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SnowyCosmetics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
